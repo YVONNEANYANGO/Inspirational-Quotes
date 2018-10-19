1a1c7b5ac6fcbc860f5a105b15d552c33efc24fa
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-10-19 13:53:09 +0300

    add constructor for creating new class properties

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Amazing Inspirational Quotes';
+
+quotes: string[];
+
+  constructor(){
+
+    this.quotes = ["Earn your success based on service to others,not at the expense of others.",
+    "You get in life what you have the courage to ask for.",
+    "If I'm gonna tell a real story,I'm gonna start with my name.",
+    "Help others achieve their dreams and you will achieve yours.",
+    "No act of kindness,no matter how small,is ever wasted.",]
+  }
 }
