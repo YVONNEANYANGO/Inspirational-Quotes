94c30a45fc0a2adb78de02d15243380d8f8e11e6
YVONNEANYANGO <ivonnahbonswuh@gmail.com>
2018-10-19 16:43:12 +0300

    add component quote-details to add new properties for details

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,11 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
 
 @Component({
   selector: 'app-quote',
   templateUrl: './quote.component.html',
   styleUrls: ['./quote.component.css']
 })
+
 export class QuoteComponent implements OnInit {
+
+  quotes = [
+    new Quote(1, 'Earn your success based on service to others,not at the expense of others.'),
+    new Quote(2, 'You get in life what you have the courage to ask for.'),
+    new Quote(3, 'If I am going to tell a real story,I am going to start with my name.'),
+    new Quote(4, 'Help others achieve their dreams and you will achieve yours.'),
+    new Quote(5, 'No act of kindness,no matter how small,is ever wasted.'),
+  ]
 
   constructor() { }
 
